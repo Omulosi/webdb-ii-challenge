868730f5709485d72fe5cc8f6d10cfc2423a8c6c
Omulosi <mulongojohnpaul@gmail.com>
2019-11-05 16:58:10 +0300

    Populate migration file for creating car-dealer table

---
+++ data/migrations/20191105164323_create-car-dealer-table.js
@@ -0,0 +1,16 @@
+
+exports.up = function(knex) {
+  return knex.schema.createTable('car-dealer', tbl => {
+    tbl.increments();
+    tbl.text('VIN').unique().notNullable();
+    tbl.text('make').notNullable();
+    tbl.text('model').notNullable();
+    tbl.decimal('mileage').notNullable();
+    tbl.string('transmission_type');
+    tbl.status('status');
+  });  
+};
+
+exports.down = function(knex) {
+  return knex.dropTableIfExists('car-dealer');
+};
