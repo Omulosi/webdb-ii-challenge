a529593296505b21934988e00fa1b95abdf02314
Omulosi <mulongojohnpaul@gmail.com>
2019-11-05 16:40:28 +0300

    update knex config file to connect to the car-dealer table

--- knexfile.js
+++ knexfile.js
@@ -5,9 +5,12 @@ module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
-      filename: './data/cars.db3'
+      filename: './data/car-dealer.db3'
     },
-    useNullAsDefault: true
+    useNullAsDefault: true,
+    migrations: {
+      directory: './data/migrations'
+    }
   },
 
 };
